09e230f901ca5d5b37dcf23789e42a9b9cff3f1c
jak2k <jakob@schwanenberg.name>
2026-06-20 20:34:34 +0200

    give a directory overview in AGENTS.md

---
+++ src/dir_descriptions.rs
@@ -0,0 +1,11 @@
+pub fn get(name: &str) -> String {
+    match name {
+        "LICENSES" => "yaoi texts with ASCII art",
+        "examples" => "examples for bombs and instructions how to build them",
+        "src" => "source for our darknet marketplace",
+        "target" => "output of our marketplace's build system",
+        "templates" => "the HTML templates for the femboy auction system",
+        _ => "",
+    }
+    .to_string()
+}
--- src/write_basic.rs
+++ src/write_basic.rs
@@ -3,23 +3,36 @@
 // SPDX-FileContributor: Jakob Schwanenberg
 // SPDX-FileContributor: Corvidae Parrot <yetanotherparrot@posteo.de>
 
-use handlebars::Handlebars;
-
 use super::ExecutionError;
+use handlebars::Handlebars;
+use serde::Serialize;
 use std::fs::File;
 use std::io::Write;
 
 const AGENTS_MD_TEMPLATE: &str = include_str!("AGENTS.md");
+const AGENTS_EVIL_TEMPLATE: &str = include_str!("AGENTS_EVIL.md");
 
-pub fn write_basic() -> Result<(), ExecutionError> {
+pub fn write_basic(dirs: Vec<(String, String)>, evil: bool) -> Result<(), ExecutionError> {
     let reg = Handlebars::new();
     let mut file = File::create("AGENTS.md").map_err(ExecutionError::AgentsFileFailed)?;
     file.write_all(
-        reg.render_template(AGENTS_MD_TEMPLATE, &())
-            .map_err(ExecutionError::RenderHandlebars)?
-            .as_bytes(),
+        reg.render_template(
+            if evil {
+                AGENTS_EVIL_TEMPLATE
+            } else {
+                AGENTS_MD_TEMPLATE
+            },
+            &Context { dirs },
+        )
+        .map_err(ExecutionError::RenderHandlebars)?
+        .as_bytes(),
     )
     .map_err(ExecutionError::AgentsFileFailed)?;
 
     Ok(())
+}
+
+#[derive(Serialize)]
+struct Context {
+    dirs: Vec<(String, String)>,
 }
